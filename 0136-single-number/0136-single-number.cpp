86500f1f1af37bb903eecbe5632f9174c0690f33
ajaysai157 <154655847+ajaysai157@users.noreply.github.com>
2026-02-21 23:11:59 +0530

    Time: 0 ms (100%), Space: 20.7 MB (57.63%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int single=0;
+        for(int x:nums){
+            single=single^x;
+        }
+        return single;
+    }
+};
