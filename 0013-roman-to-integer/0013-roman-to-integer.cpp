a73c09e5040fa8cc4e948df205443fc65ac8657a
ajaysai157 <154655847+ajaysai157@users.noreply.github.com>
2026-02-21 23:01:46 +0530

    Time: 6 ms (55.44%), Space: 13.5 MB (6.78%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        map<char, int> romanmap={
+            {'I',1},
+            {'V',5},
+            {'X',10},
+            {'L',50},
+            {'C',100},
+            {'D',500},
+            {'M',1000}
+        };
+        int total=0;
+        for(int i=0;i<s.size();i++){
+            int value=romanmap[s[i]];
+            if(i+1<s.size() && value<romanmap[s[i+1]]){
+                total-=value;
+            }else{
+                total+=value;
+            }
+        }
+        return total;
+    }
+};
