94f30de7342610cceba9be2b9075ca8907ff42f6
ajaysai157 <154655847+ajaysai157@users.noreply.github.com>
2026-02-22 09:26:59 +0530

    Time: 0 ms (100%), Space: 9.8 MB (60.01%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        if(str1+str2!=str2+str1) return "";
+        int a=str1.size(),b=str2.size();
+        while(b!=0){
+            int temp=b;
+            b=a%b;
+            a=temp;
+        }
+        return str1.substr(0,a);
+    }
+};
