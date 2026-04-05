8d0ba2d9de08684ca41b8f71373d13fd72cf6643
ajaysai157 <154655847+ajaysai157@users.noreply.github.com>
2026-04-05 14:37:36 +0530

    Time: 0 ms (100%), Space: 42.7 MB (86.7%) - LeetHub

--- 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.java
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.java
@@ -1,5 +1,13 @@
 class Solution {
     public boolean checkString(String s) {
-        return s.lastIndexOf('a')<s.indexOf('b') || s.indexOf('b')==-1;
+        boolean flag=false;;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='a'){
+                if(flag) return false;
+            }else{
+                flag=true;
+            }
+        }
+        return true;
     }
 }
