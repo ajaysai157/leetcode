51384c2f3edba428f4d2f3ec9b0da97274706580
ajaysai157 <154655847+ajaysai157@users.noreply.github.com>
2026-06-02 18:35:37 +0530

    Time: 22 ms (6.76%), Space: 42.8 MB (22.47%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,5 +1,14 @@
 class Solution {
     public int mySqrt(int x) {
-        return (int)Math.sqrt(x);
+        if(x==0) return 0;
+        int ans=1;
+        for(long i=2;i<x;i++){
+            if(i*i<=x){
+                ans=(int)i;
+            }else{
+                break;
+            }
+        }
+        return ans;
     }
 }
