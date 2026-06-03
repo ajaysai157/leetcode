a0d8c275bb8549626c0aafdab58e3c269dd137e4
ajaysai157 <154655847+ajaysai157@users.noreply.github.com>
2026-06-03 10:47:29 +0530

    Time: 5 ms (100%), Space: 50.9 MB (71.33%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.java
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.java
@@ -7,12 +7,7 @@ class Solution {
         return sum;
     }
     public int smallestDivisor(int[] nums, int threshold) {
-        int left=1,right=Integer.MIN_VALUE;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]>right){
-                right=nums[i];
-            }
-        }
+        int left=1,right=1_000_000;
         while(left<=right){
             int mid=left+(right-left)/2;
             if(sumOfDivisors(nums,mid)<=threshold){
