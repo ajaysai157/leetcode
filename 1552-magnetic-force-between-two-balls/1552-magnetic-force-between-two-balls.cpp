f21f3a9ed02d0f867cec8873e8b2b9c2c7b85a7a
ajaysai157 <154655847+ajaysai157@users.noreply.github.com>
2026-06-05 12:41:36 +0530

    Time: 47 ms (70.86%), Space: 61.8 MB (37.05%) - LeetHub

--- 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
+    bool possible(vector<int>& position,int mid,int m){
+        int cnt=1,last=position[0];
+        for(int x:position){
+            if(x-last>=mid){
+                cnt++;
+                last=x;
+            }
+        }
+        return cnt>=m;
+    }
+
     int maxDistance(vector<int>& position, int m) {
         sort(position.begin(),position.end());
-        int left=1,right=position[position.size()-1];
+        int left=1,right=position[position.size()-1]-position[0];
         while(left<=right){
             int mid=left+(right-left)/2;
-            int cnt=1,last=position[0];
-            for(int x:position){
-                if(x-last>=mid){
-                    cnt++;
-                    last=x;
-                }
-            }
-            if(cnt>=m){
+            if(possible(position,mid,m)){
                 left=mid+1;
             }else{
                 right=mid-1;
