73b883cfedc54a98a9835362d70c47a9b3749da6
ajaysai157 <154655847+ajaysai157@users.noreply.github.com>
2026-06-12 08:38:00 +0530

    Time: 5 ms (97.94%), Space: 43.8 MB (73.08%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,8 +1,15 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
+        int[] pos1 = new int[256];
+        int[] pos2 = new int[256];
         for(int i=0;i<s.length();i++){
-            if(t.indexOf(t.charAt(i)) != s.indexOf(s.charAt(i)))
+            char c1=s.charAt(i);
+            char c2=t.charAt(i);
+            if(pos1[c1] != pos2[c2]){
                 return false;
+            }
+            pos1[c1]=i+1;
+            pos2[c2]=i+1;
         }
         return true;
     }
