52c7d36f83590f7642c2f69dd65f9c1693f180e3
ajaysai157 <154655847+ajaysai157@users.noreply.github.com>
2026-06-14 22:44:46 +0530

    Time: 4 ms (65.93%), Space: 23.6 MB (95.19%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -5,11 +5,15 @@ public:
         vector<vector<int>> ans;
         sort(intervals.begin(),intervals.end());
         for(int i=0;i<n;i++){
-            if(ans.empty() || intervals[i][0]>ans.back()[1]){
-                ans.push_back(intervals[i]);
-            }else{
-                ans.back()[1]=max(ans.back()[1],intervals[i][1]);
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+            int j = i + 1;
+            while (j < n && intervals[j][0] <= end) {
+                end = max(end, intervals[j][1]);
+                j++;
             }
+            ans.push_back({start, end});
+            i = j-1;
         }
         return ans;
     }
